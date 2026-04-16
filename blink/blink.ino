0feecf903c3156681797eefddfbc9f56b39606a0
Arthur Paul Nickel <apn25@inf.ufpr.br>
2026-04-16 09:29:45 -0300

    .

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,19 +1,20 @@
-#define LED_PIN 48
+#include <Arduino.h>
 
+// O LED RGB onboard do ESP32-S3 geralmente é o pino 48
+#define RGB_BUILTIN 48
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
+  // Nenhuma configuração especial é necessária para o neopixelWrite
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("HIGH");
-  digitalWrite(LED_PIN, HIGH);
+  // neopixelWrite(pino, r, g, b) - valores de 0 a 255
+  neopixelWrite(RGB_BUILTIN, 100, 0, 0); // Vermelho (brilho baixo)
   delay(500);
-
-  Serial.println("LOW");
-  digitalWrite(LED_PIN, LOW);
+  neopixelWrite(RGB_BUILTIN, 0, 100, 0); // Verde (brilho baixo)
+  delay(500);
+  neopixelWrite(RGB_BUILTIN, 0, 0, 100); // Azul (brilho baixo)
+  delay(500);
+  neopixelWrite(RGB_BUILTIN, 0, 0, 0);   // Desligado
   delay(500);
 }
